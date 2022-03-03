ed8223fceadafb296690cdd43d1798583267440c
Jakhongir <anorboev.jahongir8007@gmail.com>
2022-03-03 11:54:13 +0500

    added NewProductInstallment and Pay methods

--- storage/database/database.go
+++ storage/database/database.go
@@ -28,3 +28,26 @@ func (d Database) GetInfo(productName, customerPhone string,price float64,instal
 	return paidPart,nil
 
 }
+
+func (d Database) NewProductInstallment(id,productName, customerPhone string,productPrice, additionPrice float64, productCategory,installmentRange int) error {
+	query := `insert into product_installment (id, product_name,customer_phone, categoty_id, product_price, addition_price, installment_range) values ($1,$2,$3,$4,$5,$6,$7)`
+
+	_,err := d.db.Exec(query,id,productName,customerPhone,productCategory,productPrice,additionPrice,installmentRange)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// this method returns remaining amount of debt and error, if there is error it returns -1
+func (d Database) Pay(productName, customerPhone string,installmentDuration int, payingAmount float64) (float64,error) {
+	var paidPart, originalPrice float64
+	query := `update product_installment set paid_part = paid_part + $1 where product_name = $2, customer_phone = $3, installment_range = $4 returning paid_part, product_price`
+
+	err := d.db.QueryRow(query,payingAmount,productName,customerPhone,installmentDuration).Scan(&paidPart,originalPrice)
+	if err != nil {
+		return -1, err
+	}
+
+	return originalPrice - paidPart,nil
+}
