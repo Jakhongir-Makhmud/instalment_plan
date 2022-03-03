a3a95fea339341ad1adcb5f3bd6ac5ea42a96d10
Jakhongir <anorboev.jahongir8007@gmail.com>
2022-03-03 10:56:15 +0500

    api setup completed

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -1,2 +1,15 @@
 package handlers
 
+import (
+	"instalment_plan/storage/repo"
+)
+
+type handlers struct {
+	db repo.DatabaseRepo
+}
+
+func NewHandler(db repo.DatabaseRepo) *handlers {
+	return &handlers{
+		db: db,
+	}
+}
--- api/router.go
+++ api/router.go
@@ -1,3 +1,28 @@
 package api
 
+import (
+	"instalment_plan/config"
+	"instalment_plan/storage/repo"
 
+	"github.com/gin-gonic/gin"
+)
+
+// Option ...
+type Option struct {
+	Conf config.Config
+	db   repo.DatabaseRepo
+}
+
+// New ...
+func New(option Option) *gin.Engine {
+	router := gin.Default()
+
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	// handler := handlers.NewHandler(option.db)
+
+	// router.GET("/",)
+
+	return router
+}
